61f8a12ce3bb9546d0195d0d4dcaaaa115777fea
RahulDas20 <rahulbd6546@gmail.com>
2024-08-16 08:56:57 +0600

    added separated data

--- data/reshape.R
+++ data/reshape.R
@@ -12,4 +12,17 @@ new_tidy_data %>%
   ggplot(aes(year,fertility)) +
   geom_point(aes(color=country))
 
-new_wide_data <- 
+new_wide_data <- new_tidy_data %>% spread(year, fertility)
+new_wide_data
+
+
+filename <- "life-expectancy-and-fertility-two-countries-example.csv"
+filename <- file.path(path,filename)
+
+raw_dat <- read_csv(filename)
+raw_dat
+
+
+dat <- raw_dat %>% gather(key,value, -country)
+dat %>% separate(key,c("year","variable_name"),"_")
+new <- dat %>% separate(key, c("year", "variable_name"))
