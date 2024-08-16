83d335871cc5b62e634e7a87b63246f834da5e29
RahulDas20 <rahulbd6546@gmail.com>
2024-08-16 08:42:00 +0600

    adding wide data and tidy data

--- data/reshape.R
+++ data/reshape.R
@@ -1,1 +1,15 @@
+library(tidyverse)
+library(dslabs)
 
+path <- system.file("extdata", package = "dslabs")
+
+filename <- file.path(path,"fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+wide_data
+new_tidy_data <- gather(wide_data, year, fertility, -country, convert = TRUE)
+
+new_tidy_data %>%
+  ggplot(aes(year,fertility)) +
+  geom_point(aes(color=country))
+
+new_wide_data <- 
